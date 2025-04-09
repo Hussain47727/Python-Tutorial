3936766f021da6caa7513bf4c6b509b0e099f517
Hussain <hsrar388@gmail.com>
2025-04-09 01:15:09 -0700

    first commit

---
+++ First-Lecture/First-Lecture.py
@@ -0,0 +1,28 @@
+#check version
+import sys
+
+print(sys.version)
+
+# write first programe print hello world
+print("hello world")
+print("my name is israr Hussain")
+print("hello world", "my name is israr Hussain")
+
+# variable .... how to store value in a variable and also check the
+# the data type
+
+first = "Hello world"
+name = "my name is israr Hussain"
+age = 20
+price = 3.33
+old = False
+a = None
+
+print(first, name, "and iam ", age, "years old")
+
+print(type(first))
+print(type(age))
+print(type(price))
+print(type(old))
+print(type(a))
+
