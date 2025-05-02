ead3a1874f0e7a7459979409cb26dcd28fc8a2ab
Hussain <hsrar388@gmail.com>
2025-05-02 09:51:49 -0700

    'updated'

--- Second-Lecture/second.py
+++ Second-Lecture/second.py
@@ -1,4 +1,12 @@
 #string
 """Today in this lecture we will learn about strings
 string is a datatype that store a squence of characters
+string is stored in the quots maybe its single, doulbe or thripple
 """
+str1 = 'This is single string'
+str2 = "This is single string" 
+str3 = "'This is single string'"
+
+    # How we purform different operation in string.. Basic operations
+    # 1. concatination
+    # "hello" + "world" = "helloworld"
