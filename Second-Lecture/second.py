370b06de69c115aa48f610bf8640cc044fd2828a
Hussain <hsrar388@gmail.com>
2025-05-10 23:52:57 -0700

    'hello'

--- Second-Lecture/second.py
+++ Second-Lecture/second.py
@@ -20,3 +20,14 @@ were ending index is not included"""
 
 str = "iamhussainisrar"
 print(str[3:10])
+
+# Practice
+# Write a programe to input user first name and prints its length
+
+userName = input("Enter your name ")
+print(len(userName))
+
+# Write a progrmae to find the occurance of $ in the string 
+
+st1 = "hi $iam israr $hussain and i get 400$ salary in five year"
+print(st1.count("$"))
