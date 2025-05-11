fa04892a4f6b837d31134abccbce6c49d2080826
Hussain <hsrar388@gmail.com>
2025-05-11 03:03:58 -0700

    hello

---
+++ Second-Lecture/conditional-statement.py
@@ -0,0 +1,18 @@
+#conditioal statement
+"""if-elif-else(syntex)
+if(condition):
+statement1
+elif(condition):
+statement2
+else:
+statement N
+"""
+age = 20
+
+if(age >= 19):
+    print("can vote and apply for licences")
+elif(age < 19):
+    print("you are not able to vote")
+else:
+    print("enter correct please")
+
