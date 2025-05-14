363109769b7d40717a6b1e2b18b8bdc8725c68f5
Hussain <hsrar388@gmail.com>
2025-05-14 09:57:40 -0700

    //

--- Third-Lecture/List-And-Tupples.py
+++ Third-Lecture/List-And-Tupples.py
@@ -4,21 +4,4 @@
 """
 
 
-class Pet:
-    def __init__(self, name1, age):
-        self.name =name1
-        self.age = age
-
-#subclass
-class Cat(Pet):
-    def __init__(self, name1, age, breed):
-        super().__init__(name1, age)
-        self.breed =breed
-
-    def bark(self):
-        print("bark")
-
-class Dog(Pet):
-    def purr(self):
-        print("purr purr")
 
