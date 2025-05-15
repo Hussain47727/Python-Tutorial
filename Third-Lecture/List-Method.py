087ebf9bfc504b456ea073743685052dab469404
Hussain <hsrar388@gmail.com>
2025-05-15 01:34:20 -0700

    'insert'

--- Third-Lecture/List-Method.py
+++ Third-Lecture/List-Method.py
@@ -3,13 +3,32 @@
 List = [2, 1, 3]
 print(List)
 
-#List append 
+# 1.List append 
 # by using list append we can add an element in the end of list.. for example we add 4
 List.append(4)
 print(List)
 
-#List sort
+# 2.List sort
 # By using list sort we arrange the element in order like acending to decending
+# First we print in ascending
 
 print(List.sort())
 print(List)
+
+# now in decending
+print(List.sort(reverse=True))
+print(List)
+
+# 3. List.reverse
+# List.reverse is basically used to reverse all the list 
+print(List.reverse())
+print(List)
+
+# 4. List.insert
+# it is similar to list.append but it insert element in a particular index
+
+list.insert(2, "ahmad")
+print(List)
+
+
+
