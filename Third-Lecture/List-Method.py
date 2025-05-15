1da1b6331d42c1c241106a50ad919bf709610ece
Hussain <hsrar388@gmail.com>
2025-05-15 01:20:30 -0700

    'updated'

---
+++ Third-Lecture/List-Method.py
@@ -0,0 +1,15 @@
+# There are some basic method or fuction related to funtion we discuss below
+
+List = [2, 1, 3]
+print(List)
+
+#List append 
+# by using list append we can add an element in the end of list.. for example we add 4
+List.append(4)
+print(List)
+
+#List sort
+# By using list sort we arrange the element in order like acending to decending
+
+print(List.sort())
+print(List)
