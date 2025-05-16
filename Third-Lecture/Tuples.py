3a3473dd7180498288813bae02acb39e50104852
Hussain <hsrar388@gmail.com>
2025-05-15 23:06:17 -0700

    'updated'

---
+++ Third-Lecture/Tuples.py
@@ -0,0 +1,7 @@
+ # Tuples
+# A built-in data type that lets us create immutable sequences of values.
+# A tuple is basically same as list but there is bit difference list is mutable
+# and tuple is immutable.
+
+tup = (2, 3, 5, 6)
+print(tup)
