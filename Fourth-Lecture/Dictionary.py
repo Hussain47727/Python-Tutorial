9f28235e8db824548672e5c4714bdf75e578aab9
Hussain <hsrar388@gmail.com>
2025-05-19 05:06:17 -0700

    'updated'

--- Fourth-Lecture/Dictionary.py
+++ Fourth-Lecture/Dictionary.py
@@ -3,3 +3,12 @@
 #Dictionaries are used to store data values in key : value pairs
 
 #They are unordered, mutable(changeable) and don't allow duplicate keys
+
+dic = {
+    "name" : "israr Hussain",
+    "age" : 12,
+    "class" : "13 year",
+    "subjects" : ["opps", "dld", "database", "linear algebra", "multivariable claclus"]
+    }
+print(dic)
+print(type(dic))
