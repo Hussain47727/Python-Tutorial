1005ce74bc6df63bd5d8a77c20bf1381cea77472
Hussain <hsrar388@gmail.com>
2025-05-19 10:11:04 -0700

    methods in dic

--- Fourth-Lecture/Dic-Methods.py
+++ Fourth-Lecture/Dic-Methods.py
@@ -11,3 +11,26 @@ student = {
 }
 print(student)
 print((student.keys()))
+
+# myDic.values()
+# This method returns all the values from the dictionary... for example
+
+print(student.values())
+print(list(student.values()))
+
+# myDic.items()
+# This method ruturn all (key : values) pairs as tuple
+
+print(student.items())
+print(list(student.items()))
+
+# myDic.get("key")
+# This method return the key according to value
+
+print(student.get("name"))
+
+# myDic.update("new key")
+# This method is used to add a new pair in dictionary
+
+print(student.update({"gender" : "male"}))
+print(student)
