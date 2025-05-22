79914f6a4a9bb7abcb4f94cb264aa3504a2a335a
Hussain <hsrar388@gmail.com>
2025-05-21 21:07:40 -0700

    'new'

---
+++ Fourth-Lecture/Sets.py
@@ -0,0 +1,9 @@
+ # Sets 
+# Sets is the collection of unordered items.
+# Each element is the set must be unique and immutable.
+
+# syntex : variable name = {}
+
+collection = {1, 2, 3, 4, 5}
+print(collection)
+print(type(collection))
