4d1b27f7a91b3db01f3ffa5ed2151e5f6df5a11d
Hussain <hsrar388@gmail.com>
2025-05-22 08:43:39 -0700

    'set'

--- Fourth-Lecture/Sets.py
+++ Fourth-Lecture/Sets.py
@@ -4,6 +4,13 @@
 
 # syntex : variable name = {}
 
-collection = {1, 2, 3, 4, 5}
+collection = {1, 2, 2, 2, 3, 4, 5, "hello", "world"}
 print(collection)
 print(type(collection))
+
+# repeated elements stored only once, so it resolved to {1, 2}
+# for example if we have a set {1, 2, 2} it will print {1 ,2} only not 2 times 2
+
+empty = set() # empty set : syntex
+print(empty)
+print(type(empty))
