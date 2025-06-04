1a27a9154579c434f2e42946da3a039deaac3d7a
Hussain <hsrar388@gmail.com>
2025-06-04 10:39:29 -0700

    'key'

---
+++ Fifth-Lecture/keywords.py
@@ -0,0 +1,14 @@
+ # BREAK AND CONTINUE 
+
+#  1.. BREAK : used to terminate the loop when encountered.
+
+i = 1
+while i <= 5:
+    print(i)
+    if( i == 3):
+        break
+    i += 1
+print('end of loop ')
+
+# 2.. CONTINUE : Terminate execution in the current iteration and continue of
+# the loop with the next iteration
