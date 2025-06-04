c0638041123a12939410ab32204e8bf0634e07fb
Hussain <hsrar388@gmail.com>
2025-06-04 10:53:47 -0700

    'new'

--- Fifth-Lecture/keywords.py
+++ Fifth-Lecture/keywords.py
@@ -12,3 +12,21 @@ print('end of loop ')
 
 # 2.. CONTINUE : Terminate execution in the current iteration and continue of
 # the loop with the next iteration
+
+j = 1 
+while j <= 5:
+    if(j == 2):
+        j += 1
+        continue
+    print(j)
+    j += 1
+print('j loop ending')
+
+even = 1
+while even <= 10:
+    if(even % 2 == 0):
+        even += 1
+        continue
+    print(even)
+    even += 1
+        
