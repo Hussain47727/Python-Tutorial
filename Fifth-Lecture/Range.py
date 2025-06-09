f9af624d49d0e12a6590ab30c2a23824a905c49f
Hussain <hsrar388@gmail.com>
2025-06-09 06:09:16 -0700

    'range'

---
+++ Fifth-Lecture/Range.py
@@ -0,0 +1,14 @@
+  # RANGE 
+
+print(range(10))
+
+ # IF WE WANT TO PRINT 0 TO 9
+for i in range(10):
+    print(i)
+# IF WE WANT TO PRINT 2 , 9
+for i in range(2 , 10):
+    print(i)
+
+# IF WE WANT TO PRINT EVEN NUMBER OR MULTIPLE OF 2
+for i in range(2, 10, 2):
+    print(i)
