8a478b3babeeaa97af30d3dfe755c4253304e90e
Hussain <hsrar388@gmail.com>
2025-06-09 06:21:05 -0700

    'push'

--- Fifth-Lecture/Range.py
+++ Fifth-Lecture/Range.py
@@ -12,3 +12,16 @@ for i in range(2 , 10):
 # IF WE WANT TO PRINT EVEN NUMBER OR MULTIPLE OF 2
 for i in range(2, 10, 2):
     print(i)
+
+           #PRACTICE QUESTIONS
+# Q1..PRINT NUMBER FROM 1 TO 100.
+for i in range(1, 101):
+    print(i)
+
+# Q2..PRINT NUMBER FROM 100 T0 1.
+for i in range(100, 0, -1):
+    print(i)
+
+# Q3..PRINT THE MULTIPLICATION TABLE OF A NUMBER N.
+for i in range(3, 30, 3):
+    print(i)
