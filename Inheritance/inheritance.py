39d45cc82d791bc61e2f2f597188f2f494bc4027
Hussain <hsrar388@gmail.com>
2025-06-23 02:18:13 -0700

    'inheritance'

--- Inheritance/inheritance.py
+++ Inheritance/inheritance.py
@@ -1,35 +1,35 @@
-# inheritance mean use parant class features into child class
+# # inheritance mean use parant class features into child class
 
-class Animal:
-    def speak(self):
-        print("Animal is speaking")
+# class Animal:
+#     def speak(self):
+#         print("Animal is speaking")
         
 
-class Dog(Animal):
-    def bark(self):
-        print("Dog is barking")
+# class Dog(Animal):
+#     def bark(self):
+#         print("Dog is barking")
 
-d = Dog()
-d.speak()   # Inherited method
-d.bark() 
+# d = Dog()
+# d.speak()   # Inherited method
+# d.bark() 
 
-# short exampel
-class vehical:
-    def __init__(self, brand):
-        self.brand = brand
-    def show_brand(self):
-        print('brand : toyota')
+# # short exampel
+# class vehical:
+#     def __init__(self, brand):
+#         self.brand = brand
+#     def show_brand(self):
+#         print('brand : toyota')
 
-class car(vehical):
-    def __init__(self, brand, model):
-        super().__init__(brand)
-        self.model = model
-    def show_model(self):
-        print("model : corolla")
+# class car(vehical):
+#     def __init__(self, brand, model):
+#         super().__init__(brand)
+#         self.model = model
+#     def show_model(self):
+#         print("model : corolla")
     
 
-car1 = car("Toyota", "Corolla")
-car1.show_brand()
-car1.show_model()
+# car1 = car("Toyota", "Corolla")
+# car1.show_brand()
+# car1.show_model()
 
         
