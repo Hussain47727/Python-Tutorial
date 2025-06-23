7a5c5aad8501a72c777456c5b7b893458a77f98a
Hussain <hsrar388@gmail.com>
2025-06-22 22:41:38 -0700

    'method'

--- classes and objects/cls.py
+++ classes and objects/cls.py
@@ -1,47 +1,17 @@
- #classes and objects 
-#  Classes are the plueprint of creating objects
+class student:
+    def __init__(self, name, age, grade):
+        self.name = name
+        self.age = age
+        self.grade = grade
+    def show_info(self):
+        print('student name:', self.name)
+        print('student age :', self.age)
+        print('student grade', self.grade)
 
-#now we create a class and object for student
-#
+# create object 
+student1 = student('israr', 18, "A")
 
-
-#now for car 
-
-class Car :
-    color = "blue"
-    price = 3000000
-
-car1 = Car()
-print(car1.price)
-
-
-class bike :
-    model = '2025'
-    color = "blue"
-    price = 300000
-    
-bike1 = bike()
-print(bike1.color)
-#__ init __ Fuction or constructor
-# All classes have a fuction called __init__(), which is always
-# execute when the class is being initiated
-
-# class Students :
-
-#     College_name = "Uswa public School And College"
-#     def __init__ (self, name, age, marks):
-#         self.name = name
-#         self.age = age
-#         self.marks = marks
-#         print("another student..")
-#         print(Students.College_name)
-
-# s1 = Students("Ali", 20, 45)
-# print(s1.name, s1.age, s1.marks)
-# s2 = Students("Haider", 19, 34)
-# print(s1.name, s1.age, s1.marks)
-# s1 = Students("Ali", 20, 45)
-# print(s1.name, s1.age, s1.marks)
+# call method
+student1.show_info()
 
         
-
