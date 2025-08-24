70af330c1681221fc94c0ef44a199fdae8c7920f
Hussain <hsrar388@gmail.com>
2025-08-24 03:48:55 -0700

    'react'

--- start-React/first/src/App.jsx
+++ start-React/first/src/App.jsx
@@ -1,19 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+function App(){
+  const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <h1>React Counter</h1>
-      <p>count : {count}</p>
-      <button onClick={() =>setCount(count + 1)}>Increase</button>
-      <button onClick={() =>setCount(count - 1 )}>Decrease</button>
-    </>
-  )
-}
+ const Increase = ()=>{
+  if(count < 10){
+    setCount(count + 1)
+  }
+ };
 
-export default App
+ const Decrease = () => {
+  if(count > 0){
+    setCount(count - 1)
+  }
+ };
+
+  
+
+  return(
+    <div style = {{textAlign : "center", marginTop : "50px"}}>
+    <h1>React Counter</h1>
+    <p>Count : {count}</p>
+    <button onClick={Increase}>Increase</button>
+    <button onClick={Decrease}>Decrease</button>
+    </div>
+  );
+};
+
+export default App;
