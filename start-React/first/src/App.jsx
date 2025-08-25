cf5d031dd255bc4aa04b20d84ef1131a1dec3c6d
Hussain <hsrar388@gmail.com>
2025-08-24 21:47:43 -0700

    'update'

--- start-React/first/src/App.jsx
+++ start-React/first/src/App.jsx
@@ -1,30 +1,30 @@
-import { useState } from "react";
+// import { useState } from "react";
 
-function App(){
-  const [count, setCount] = useState(0);
+// function App(){
+//   const [count, setCount] = useState(0);
 
- const Increase = ()=>{
-  if(count < 10){
-    setCount(count + 1)
-  }
- };
+//  const Increase = ()=>{
+//   if(count < 10){
+//     setCount(count + 1)
+//   }
+//  };
 
- const Decrease = () => {
-  if(count > 0){
-    setCount(count - 1)
-  }
- };
+//  const Decrease = () => {
+//   if(count > 0){
+//     setCount(count - 1)
+//   }
+//  };
 
   
 
-  return(
-    <div style = {{textAlign : "center", marginTop : "50px"}}>
-    <h1>React Counter</h1>
-    <p>Count : {count}</p>
-    <button onClick={Increase}>Increase</button>
-    <button onClick={Decrease}>Decrease</button>
-    </div>
-  );
-};
+//   return(
+//     <div style = {{textAlign : "center", marginTop : "50px"}}>
+//     <h1>React Counter</h1>
+//     <p>Count : {count}</p>
+//     <button onClick={Increase}>Increase</button>
+//     <button onClick={Decrease}>Decrease</button>
+//     </div>
+//   );
+// };
 
 export default App;
